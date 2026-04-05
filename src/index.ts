1c6639381d6d7c1dc57f6d0c5570933b4b6951f4
Adrian de la Rosa <github@brick.email>
2026-04-05 14:38:34 +0200

    feat!: add EnPassantSquare type, restrict en passant to ranks 3 and 6
    BREAKING CHANGE: enPassantSquare fields now use EnPassantSquare instead of Square

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,16 @@ export type {
   CastlingRights,
   Color,
   DeriveOptions,
+  EnPassantSquare,
   File,
-  Move,
   Piece,
   PieceType,
   PositionOptions,
-  PromotionPieceType,
   Rank,
   Square,
   SquareColor,
 } from './types.js';
 
-export {
-  COLORS,
-  EMPTY_BOARD,
-  FILES,
-  PIECE_TYPES,
-  RANKS,
-  SQUARES,
-  STARTING_POSITION,
-} from './constants.js';
+export { STARTING_POSITION } from './constants.js';
 
 export { Position } from './position.js';
-
-export { squareColor, squareFile, squareRank } from './squares.js';
