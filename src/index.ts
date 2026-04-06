c4e169a9b231476362f4223053567cd07c5ec671
Adrian de la Rosa <github@brick.email>
2026-04-06 23:24:13 +0200

    refactor!: remove move constants and PieceMove from public exports

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export type {
   EnPassantSquare,
   File,
   Piece,
-  PieceMove,
   PieceType,
   PositionOptions,
   Rank,
@@ -15,14 +14,5 @@ export type {
 } from './types.js';
 
 export { STARTING_POSITION } from './constants.js';
-
-export {
-  BISHOP_MOVES,
-  KING_MOVES,
-  KNIGHT_MOVES,
-  PAWN_MOVES,
-  QUEEN_MOVES,
-  ROOK_MOVES,
-} from './moves.js';
 
 export { Position } from './position.js';
